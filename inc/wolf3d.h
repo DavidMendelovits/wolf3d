f1d4bb3ae2f7700ede60f98dc3ffd6cf0e0ca210
DavidMendelovits <David.Mendelovits@gmail.com>
2018-11-06 14:09:28 -0800

    reading maps

--- inc/wolf3d.h
+++ inc/wolf3d.h
@@ -6,7 +6,7 @@
 /*   By: dmendelo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/05 11:24:11 by dmendelo          #+#    #+#             */
-/*   Updated: 2018/11/05 11:28:42 by dmendelo         ###   ########.fr       */
+/*   Updated: 2018/11/06 14:09:19 by dmendelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,24 @@
 #include "mlx.h"
 #include <math.h>
 #include <errno.h>
+#include <sys/types.h>
+#include <fcntl.h>
+
+# define USAGE "wolf3d -> Usage:\n./wolf3d [map]\n"
+# define MAP_ERROR "Invalid Map!\n"
+typedef struct		s_point
+{
+	int				x;
+	int				y;
+}					t_point;
+
+typedef struct		s_map
+{
+	int				width;
+	int				height;
+	int				**map;
+	t_point			start;
+}					t_map;
+
 
 #endif
--- src/wolf3d.c
+++ src/wolf3d.c
@@ -6,20 +6,14 @@
 /*   By: dmendelo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/05 11:23:24 by dmendelo          #+#    #+#             */
-/*   Updated: 2018/11/05 13:46:11 by dmendelo         ###   ########.fr       */
+/*   Updated: 2018/11/06 14:09:18 by dmendelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "wolf3d.h"
 #include <stdio.h>
 
-typedef struct 	s_point
-{
-	int			y;
-	int			x;
-}				t_point;
-
-int				main(void)
+void			new_mlx(void)
 {
 	void		*mlx;
 	void		*window;
@@ -44,7 +38,7 @@ int				main(void)
 	window = mlx_new_window(mlx, 1000, 1000, "newnew");
 	for (int i = start.x; i < end.x; i++)
 	{
-		mlx_pixel_put(mlx, window, i, 10, 0xf8f8ff);
+		mlx_pixel_put(mlx, window, i, j, 0xf8f8ff);
 		if (e >= 0)
 		{
 			j += 1;
@@ -54,5 +48,80 @@ int				main(void)
 		e += m;
 	}
 	mlx_loop(mlx);
+
+}
+
+t_map			*init_map(void)
+{
+	t_map				*map;
+
+	map = (t_map *)malloc(sizeof(*map));
+	map->width = 0;
+	map->height = 0;
+	map->map = NULL;
+	map->start.x = 0;
+	map->start.y = 0;
+	return (map);
+}
+
+int				read_validate_first_line(t_map **map, int fd)
+{
+	char				*line;
+	char				*tmp;
+	char				**args;
+
+	line = NULL;
+	get_next_line(fd, &line);
+	tmp = replace_char(line, 'x', ' ');
+	args = ft_strsplit(tmp);
+	ft_print_strings(args);
+	if (ptr_count(args) != 2)
+	{
+		printf("ptr_count = %d\n", ptr_count(args));
+		return (0);
+	}
+	(*map)->width = ft_atoi(args[0]);
+	(*map)->height = ft_atoi(args[1]);
+	return (1);
+}
+
+t_map			*read_validate_map(char *filename)
+{
+	t_map				*map;
+	int					fd;
+
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (NULL);
+	map = init_map();
+	if (!read_validate_first_line(&map, fd))
+		return (NULL);
+	printf("map->width = %d\nmap->height = %d\n", map->width, map->height);
+	return (map);
+}
+
+void			wolf3d(char *mapname)
+{
+	t_map				*map;
+
+	map = read_validate_map(mapname);
+	if (!map)
+	{
+		write(2, MAP_ERROR, sizeof(MAP_ERROR));
+		return ;
+	}
+}
+
+int				main(int argc, char **argv)
+{
+	if (argc == 2)
+	{
+		wolf3d(argv[1]);
+	}
+	else
+	{
+		write(2, USAGE, sizeof(USAGE));
+	}
+//	new_mlx();
 	return (0);
 }
